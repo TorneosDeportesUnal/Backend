9718dce23444f9308211994c69fd4c17c890f14b
camilo13152 <caisazafo@unal.edu.co>
2017-03-31 00:41:28 -0500

    Match Queries added!

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,18 @@ class Match < ApplicationRecord
   
   has_many :team_matches
   has_many :matches, through: :team_matches
+
+  def self.matches()
+  	Match.all
+  end
+
+  def self.match_by_id(id_match)
+  	find_by_id(id_match)
+  end
+
+  def self.match_teams(id_match)
+  	select("matches.winner_team, matches.loser_team").from("matches").where(matches:{id: id_match})
+  end
+
+
 end
