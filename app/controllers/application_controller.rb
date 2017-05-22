38bd7b28dbed342029fb85ef3ec3e84ee1017184
Juan Camilo Lopez <juclopezso@unal.edu.co>
2017-05-22 01:03:17 -0500

    Activadas sesiones de usuarios para tournaments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  #include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :skip_session
   ## Skip sessions and cookies for Rails API
  def skip_session
