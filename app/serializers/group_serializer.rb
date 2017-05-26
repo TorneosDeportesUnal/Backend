3e48625c8b47f070526274505dd2ef2875b27524
Cristian <cdcordobaa@unal.edu.co>
2017-05-26 08:39:17 +0000

    group serializer response update, now whit teams array

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,3 +1,3 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :active,:name, :winners_number, :tournament_phase_id
+  attributes :id, :active,:name, :winners_number, :tournament_phase_id, :teams
 end
